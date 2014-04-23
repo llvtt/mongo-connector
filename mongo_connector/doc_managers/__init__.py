6a8a7bacdebdd30cb78095a906789f6df4eae256
Luke Lovett <luke.lovett@mongodb.com>
2014-04-23 23:02:10 +0000

    add DocManagerBase

--- mongo_connector/doc_managers/__init__.py
+++ mongo_connector/doc_managers/__init__.py
@@ -11,3 +11,43 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+class DocManagerBase(object):
+    """Base class for all DocManager implementations."""
+
+    def apply_update(self, doc, update_spec):
+        """Apply an update operation to a document."""
+        doc.update(update_spec.get("$set", {}))
+        for key in update_spec.get("$unset", []):
+            doc.pop(key)
+        return doc
+
+    def bulk_upsert(self, docs):
+        """Upsert each document in a set of documents.
+
+        This method may be overridden to upsert many documents at once.
+        """
+        for doc in docs:
+            self.upsert(doc)
+
+    def update(self, doc, update_spec):
+        raise NotImplementedError
+
+    def upsert(self, document):
+        raise NotImplementedError
+
+    def remove(self, doc):
+        raise NotImplementedError
+
+    def search(self, start_ts, end_ts):
+        raise NotImplementedError
+
+    def commit(self):
+        raise NotImplementedError
+
+    def get_last_doc(self):
+        raise NotImplementedError
+
+    def stop(self):
+        raise NotImplementedError
