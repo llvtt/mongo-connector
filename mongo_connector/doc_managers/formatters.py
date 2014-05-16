8494a9dcca91ff7126ed122540db5e8230ba7625
Luke Lovett <luke.lovett@mongodb.com>
2014-05-16 21:07:22 +0000

    make formatters better

--- mongo_connector/doc_managers/formatters.py
+++ mongo_connector/doc_managers/formatters.py
@@ -9,6 +9,9 @@ if PY3:
 
 
 class DocumentFormatter(object):
+    """Interface for classes that can transform documents to conform to
+    external drivers' expectations.
+    """
 
     def transform_value(self, value):
         """Transform a leaf-node in a document.
@@ -33,6 +36,9 @@ class DocumentFormatter(object):
 
 
 class DefaultDocumentFormatter(DocumentFormatter):
+    """Basic DocumentFormatter that preserves numbers, base64-encodes binary,
+    and stringifies everything else.
+    """
 
     def __init__(self):
         # Map type -> transformation function for that type
@@ -58,40 +64,71 @@ class DefaultDocumentFormatter(DocumentFormatter):
 
     def format_document(self, document):
         def _kernel(doc):
-            for key, value in document.items():
+            for key in document:
+                value = document[key]
                 yield self.transform_element(key, value)
         return dict(_kernel(document))
 
 
 class DocumentFlattener(DefaultDocumentFormatter):
+    """Formatter that completely flattens documents and unwinds arrays:
+
+    An example:
+      {"a": 2,
+       "b": {
+         "c": {
+           "d": 5
+         }
+       },
+       "e": [6, 7, 8]
+      }
+
+    becomes:
+      {"a": 2, "b.c.d": 5, "e.0": 6, "e.1": 7, "e.2": 8}
+
+    """
+
+    def transform_value(self, value):
+        if isinstance(value, dict):
+            return self.transform_document(value)
+        elif isinstance(value, list):
+            return [self.transform_value(v) for v in value]
+        elif isinstance(value, (float, long, int)):
+            return value
+        elif isinstance(value, bson.Binary):
+            return base64.b64encode(value)
+        return str(value)
 
     def transform_element(self, key, value):
         if isinstance(value, list):
             for li, lv in enumerate(value):
                 for inner_k, inner_v in self.transform_element(
-                        "%s.%d" % (key, li), lv):
+                        "%s.%s" % (key, li), lv):
                     yield inner_k, inner_v
         elif isinstance(value, dict):
-            transformd = self.format_document(value)
-            for doc_key in transformd:
-                yield "%s.%s" % (key, doc_key), transformd[doc_key]
+            formatted = self.format_document(value)
+            for doc_key in formatted:
+                yield "%s.%s" % (key, doc_key), formatted[doc_key]
         else:
             yield key, self.transform_value(value)
 
     def format_document(self, document):
         def flatten(doc, path):
-            for k, v in doc:
+            top_level = (len(path) == 0)
+            if not top_level:
+                path_string = ".".join(path)
+            for k in doc:
                 v = doc[k]
-                path.append(k)
                 if isinstance(v, dict):
+                    path.append(k)
                     for inner_k, inner_v in flatten(v, path):
                         yield inner_k, inner_v
+                    path.pop()
                 else:
-                    transformd = self.transform_element(k, v)
-                    for new_k, new_v in transformd:
-                        if len(path) > 1:
-                            yield ".".join(path), new_v
-                        else:
+                    transformed = self.transform_element(k, v)
+                    for new_k, new_v in transformed:
+                        if top_level:
                             yield new_k, new_v
-                path.pop()
+                        else:
+                            yield "%s.%s" % (path_string, new_k), new_v
         return dict(flatten(document, []))
